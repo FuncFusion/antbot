4e5edb08d30d5d4f636315f5c4fa5a82bfb665aa
bth123 <m1207pro@gmail.com>
2024-01-16 18:58:12 +0200

    added say command

--- cogs/general.py
+++ cogs/general.py
@@ -5,7 +5,10 @@ from discord import app_commands
 
 class GeneralCommands(commands.Cog):
 	def __init__(self, bot):
-		@bot.tree.command(name="сказать", description="Отправить от лица Антобота")
-		@app_commands.describe(text="Текст, который будет отправлен ботом")
-		async def say(interaction: discord.Interaction, text: str):
-			await interaction.response.send_message(text)
+		
+		@bot.hybrid_command(aliases=["сказать", "молвить"])
+		async def say(ctx, *, text: str):
+			temp = await ctx.send("_ _")
+			await ctx.channel.send(text)
+			await temp.delete()
+			await ctx.message.delete()
