82db29c2d5ffaea07cf2fa35850b692a1462c297
SuperAnt220 <zp-a@i.ua>
2024-01-16 12:39:20 +0200

    logging setup

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import settings
 import discord
 from discord.ext import commands
 
+logger = settings.logging.getLogger("bot")
+
 def run():
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.event
     async def on_ready():
-        print(bot.user)
-        print(bot.user.id)
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
 
     
-    bot.run(settings.DISCORD_API_SECRET)
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
     run()
