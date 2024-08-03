7e21b8b64cb41498f678e8e1953783e3e40b534e
SuperAnt220 <zp-a@i.ua>
2024-08-04 00:20:20 +0300

    🙏 starter message for creations forum chnl

--- cogs/help/start_message.py
+++ cogs/help/start_message.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-from settings import HELP_FORUM_ID
+from settings import HELP_FORUM_ID, CREATIONS_FORUM_ID
 
 from utils.msg_utils import Emojis
 from utils.shortcuts import  no_color
@@ -16,6 +16,16 @@ class StartMessage(commands.Cog):
 				description=f"Если ещё не читали, прочитайте в закреп ветке (https://discord.com/channels/914772142300749854/1021488153909018704) \
 				рекомендации к веткам помощи, и о том, как работают некоторые её аспекты. Следование всем рекомендациям \
 				(особенно 4 пункту) поможет получить наиболее эффективную помощь. Когда проблема решится, используйте команду </resolve:1250486582109274206>.".replace("\t", ""))
+			#
+			await trd.send(embed=embed)
+			await trd.starter_message.pin()
+		if trd.parent_id == CREATIONS_FORUM_ID:
+			# Building embed
+			embed = discord.Embed(title=f"{Emojis.pin} Ознакомтесь с правилами творчества!", color=no_color, 
+				description=f"Надеемся, что вы уже прочитали правила творчества (https://discord.com/channels/914772142300749854/1142473873200267314/1142473873200267314). \
+				Если вы хотите поменять картинку на обложке вашего поста, вы **можете это сделать**. Вы можете вставитьпрямую \
+				ссылку на картинку или ссылку на тот сайт, что уже имеет картинку в своём эмбеде. Также не забывайте, что вы можете \
+				закреплять сообщения в своей ветке, отреагировав с помощью эмодзи :pin:.".replace("\t", ""))
 			#
 			await trd.send(embed=embed)
 			await trd.starter_message.pin()
