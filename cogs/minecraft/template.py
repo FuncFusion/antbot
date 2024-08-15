ca87d5ff89802b996eae51043d026b766480c924
bth123 <m1207pro@gmail.com>
2024-08-15 21:20:09 +0300

    📦Improved template generator

--- cogs/minecraft/template.py
+++ cogs/minecraft/template.py
@@ -9,7 +9,7 @@ from utils.msg_utils import Emojis
 from utils.validator import validate
 
 from utils.shortcuts import no_ping
-from utils.pack_generator import Modals
+from utils.pack_generator import Modals, PGenerator
 
 
 class TemplateCommand(commands.Cog):
@@ -20,37 +20,29 @@ class TemplateCommand(commands.Cog):
 		help="После введения данной команды с аргументом `настраиваемый` у вас вылезет окно, где можно указать следующие аргументы настраиваемого пака: `<название пака> [неймспейсы] [включить папки в пак (function/loot_table) (все по умолчанию)] [исключить папки в паке (function/loot_table)] [версия (последняя по умолчанию)]`\n### Пример:\n`/template custom - bth function loot_table damage_type - -`")
 	@app_commands.describe(template="Датапак/ресусрпак", type="Тип пака")
 
-	async def template(self, ctx, template:Literal["Датапак", "Ресурспак"]="datapack", 
-		type:Literal["Базовый", "Расширенный", "Настраиваемый"]="basic"):
+	async def template(self, ctx, template="datapack", type="basic"):
 		pack_ctx = {
-			"datapack": {"emoji": Emojis.deta_rack, "accusative": "датапака", "modal": Modals.DP},
-			"resourcepack": {"emoji": Emojis.resource_rack, "accusative": "ресурспака", "modal": Modals.RP}
+			"datapack": {"emoji": Emojis.deta_rack, "accusative": "датапака", "modal": Modals.DP, "function": PGenerator.datapack},
+			"resourcepack": {"emoji": Emojis.resource_rack, "accusative": "ресурспака", "modal": Modals.RP, "function": PGenerator.resourcepack}
 		}
 		#Validating args
-		if (v_template:=validate(template, {"datapack": ["dp", "дп", "датапак", "детарак", "патадак"], "resourcepack": ["rp", "рп", "ресурспак", "репуксрак"]}))\
-			!= None:
-			template = v_template
-		else:
-			raise commands.BadArgument(template)
-		if (v_type:=validate(type, {"basic": ["базовый", "стандартный", "обычный"], "extended": ["расширенный", "полный"], "custom": ["кастомынй", "настраиваемый"]}))\
-			!= None:
-			type = v_type
-		else:
-			raise commands.BadArgument(type)
-		for arg in [template, type]:
-			if arg == None:
-				raise commands.BadArgument(f"{arg}")
+		if (template:=validate(template, {"datapack": ["dp", "дп", "датапак", "детарак", "патадак"], 
+			"resourcepack": ["rp", "рп", "ресурспак", "репуксрак"]})) == None:
+			raise commands.BadArgument("")
+		if (type:=validate(type, {"basic": ["базовый", "стандартный", "обычный"], "extended": ["расширенный", "полный"], 
+			"custom": ["кастомынй", "настраиваемый"]})) == None:
+			raise commands.BadArgument("")
 		#
 		if type == "custom":
 			await ctx.interaction.response.send_modal(pack_ctx[template]["modal"]())
 		elif type == "extended":
-			with open(f"assets/templates/Extended {template}.zip", "rb") as pack:
-				await ctx.reply(f"{pack_ctx[template]["emoji"]} Расширенный шаблон {pack_ctx[template]["accusative"]}", 
-				file=discord.File(pack, filename=f"Extended_{template}_(UNZIP).zip"), allowed_mentions=no_ping)
+			await ctx.reply(f"## {pack_ctx[template]["emoji"]} Расширенный шаблон {pack_ctx[template]["accusative"]}", 
+				file=discord.File(pack_ctx[template]["function"](), filename=f"Extended_{template}_(UNZIP).zip"), 
+				allowed_mentions=no_ping)
 		elif type == "basic":
-			with open(f"assets/templates/Basic {template}.zip", "rb") as pack:
-				await ctx.reply(f"{pack_ctx[template]["emoji"]} Базовый шаблон {pack_ctx[template]["accusative"]}", 
-				file=discord.File(pack, filename=f"Basic_{template}_(UNZIP).zip"), allowed_mentions=no_ping)
+			await ctx.reply(f"## {pack_ctx[template]["emoji"]} Базовый шаблон {pack_ctx[template]["accusative"]}", 
+				file=discord.File(pack_ctx[template]["function"](folders_include=["function"]), 
+				filename=f"Basic_{template}_(UNZIP).zip"), allowed_mentions=no_ping)
 
 	@template.error
 	async def template_error(self, ctx: commands.Context, error):
@@ -62,6 +54,10 @@ class TemplateCommand(commands.Cog):
 			{
 				"exception": commands.BadArgument,
 				"msg": "Неверный аргумент"
+			},
+			{
+				"contains": "AttributeError",
+				"msg": "Для генерации кастомного пака, используйте **слэш** команду </template:1211773146378805328>"
 			}
 		])
 
