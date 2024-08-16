b657fe49475f76d983780427f3ce4a063b68d03e
SuperAnt220 <zp-a@i.ua>
2024-08-16 19:23:06 +0300

    ❔FAQ system optimized, 3.5 new faqs, closest_match tweak

--- cogs/faqs/__init__.py
+++ cogs/faqs/__init__.py
@@ -6,22 +6,20 @@ from settings import DMS_LOGS_GUILD_ID
 
 from Levenshtein import distance
 import re
+import os
 from typing import List
 import json
 
 from utils.general import handle_errors
 from utils.msg_utils import Emojis
 from utils.shortcuts import no_ping, no_color
-from utils.validator import all_valid
+from utils.validator import all_valid, closest_match
 
 
 with open("cogs/faqs/faqs.json", 'r', encoding="utf-8") as file: db = json.load(file)
 faq_names = sorted(list(db.keys()))
-faq_list = list(db.keys())
-for value in db.values():
-	faq_list.extend(value.get('aliases', []))
 
-offered_faqs = [app_commands.Choice(name=faq, value=faq) for faq in faq_list[:25]]
+offered_faqs = [app_commands.Choice(name=faq, value=faq) for faq in faq_names[:25]]
 
 
 class FAQs(commands.Cog, name="FAQ команды"):
@@ -44,8 +42,8 @@ class FAQs(commands.Cog, name="FAQ команды"):
 			embed.add_field(name="Как использовать факьюшки?", value="Чтобы вызвать ответ на какую либо факьюшку, напишите вопросительный знак и после него название факьюшки. Вы также можете вызвать факьюшку всередине сообщения, сделав вопросительный знак жирным. Примеры:\n`?логи`\n`Тебе стоит открыть **?**логи, потому что в них полезная инфа`")
 			await ctx.reply(embed=embed, allowed_mentions=no_ping)
 		else:
-			faq = get_faq(name)
-			aliases = ", ".join([f"`{alias}`" for alias in db[faq]['aliases']])             
+			faq = closest_match(name, db, accuracy=3)
+			aliases = ", ".join([f"`{alias}`" for alias in db[faq]])             
 			embed.title = f"{Emojis.txt} Список алиасов для \"{faq}\""
 			embed.add_field(name="", value=aliases, inline=False)
 			await ctx.reply(embed=embed, allowed_mentions=no_ping)
@@ -80,42 +78,28 @@ class FAQs(commands.Cog, name="FAQ команды"):
 			words = re.findall(r'\S+', segment)
 			for i in range(len(words)):
 				args.append(' '.join(words[:i+1]))
-		faq_names = []
+		faq = None
 		for arg in args:
-			faq_name = get_faq(arg)
-			if faq_name != None and faq_name not in faq_names:
-				faq_names.append(faq_name)
-		if len(faq_names) > 3:
-			await msg.channel.send(f"{Emojis.chat_type_open} Чтобы предотвратить флуд, я не буду отправлять больше трёх ответов за раз.", reference = msg, allowed_mentions = no_ping)
-		for faq in faq_names:
-			if faq_names.index(faq) < 3:
-				files = [discord.File(f'assets/faqs/{faq}/{file_name}') for file_name in db[faq].get("files", [])]
-				with open(f'assets/faqs/{faq}/{faq}.md', 'r', encoding="utf-8") as file: content = file.read()
-				emoji_instance = Emojis()
-				for attr in dir(emoji_instance):
-					if not attr.startswith("__") and not callable(getattr(emoji_instance, attr)):
-						content = content.replace("{" + attr + "}", getattr(emoji_instance, attr))
-				answers = content.split("\n---separator---\n")
-				for answer in answers:
-					if len(answers) == 1:
-						await msg.channel.send(answer, files=files, reference=msg, allowed_mentions=no_ping)
-					elif answers.index(answer) == 0:
-						await msg.channel.send(answer, reference=msg, allowed_mentions=no_ping)
-					elif answer != answers[-1]:
-						await msg.channel.send(answer, allowed_mentions=no_ping)
-					else:
-						await msg.channel.send(answer, files=files, allowed_mentions=no_ping)
-						
-def get_faq(arg):
-	for faq in faq_list:
-		if distance(arg, faq) <= len(faq)/3:
-			if faq not in faq_names:
-				return get_root_obj(faq)
+			faq_name = closest_match(arg, db, accuracy=3)
+			if faq_name != None:
+				faq = faq_name
+		files = []
+		for filename in os.listdir(f"assets/faqs/{faq}"):
+			if not filename.endswith(".md"):
+				files.append(discord.File(f'assets/faqs/{faq}/{filename}'))
+		with open(f'assets/faqs/{faq}/{faq}.md', 'r', encoding="utf-8") as file: content = file.read()
+		emoji_instance = Emojis()
+		for attr in dir(emoji_instance):
+			if not attr.startswith("__") and not callable(getattr(emoji_instance, attr)):
+				content = content.replace("{" + attr + "}", getattr(emoji_instance, attr))
+		answers = content.split("\n---separator---\n")
+		for answer in answers:
+			if len(answers) == 1:
+				await msg.channel.send(answer, files=files, reference=msg, allowed_mentions=no_ping)
+			elif answers.index(answer) == 0:
+				await msg.channel.send(answer, reference=msg, allowed_mentions=no_ping)
+			elif answer != answers[-1]:
+				await msg.channel.send(answer, allowed_mentions=no_ping)
 			else:
-				return faq
-
-def get_root_obj(alias):
-	for key, value in db.items():
-		if alias in value.get("aliases",[]):
-			return key
-	return None
+				await msg.channel.send(answer, files=files, allowed_mentions=no_ping)
+						
