1c86378080a51354e4540f5e1d02177ec54c6a8b
SuperAnt220 <zp-a@i.ua>
2024-08-22 16:46:25 +0300

    🙋‍♂️ Bot ping finished i think

--- cogs/general/bot_ping.py
+++ cogs/general/bot_ping.py
@@ -12,11 +12,11 @@ class BotPing(commands.Cog):
 	async def main(self, msg):
 		if msg.author == self.bot.user:
 			return
-		if msg.content.startswith("<@1196573758757028021>"):
+		if msg.content == ("<@1196573758757028021>"):
 			embed = discord.Embed(
-				description=f"Привет! Я многофункциональный дискорд бот, созданный <@536441049644793858> и <@567014541507035148> и предназначенный чисто для этого сервера, с главной целью помочь вам в изучении команд, датапаков и ресурспаков, а также имею и другие интересные функции. Чтобы узнать больше о моих функциях, используйте команду </help:1270684227419246623>", 
+				description=f"Привет! Я многофункциональный дискорд бот, созданный <@536441049644793858> и <@567014541507035148> и предназначенный чисто для этого сервера Anthill. Моя главная цель - помочь вам в изучении команд, датапаков и ресурспаков, но я также имею и другие интересные функции. Используйте команду </help:1270684227419246623>, чтобы узнать больше о моих функциях.", 
 				color=no_color)
-			embed.set_author(name="AntBot", icon_url=self.bot.user.avatar.url)
+			#embed.set_author(name="AntBot", icon_url=self.bot.user.avatar.url)
 			embed.set_thumbnail(url=self.bot.user.avatar.url)
 			await msg.reply(embed=embed, view=BotPingView(), allowed_mentions=no_ping)
 
@@ -27,5 +27,10 @@ class BotPingView(discord.ui.View):
 			label="Исходный код",
 			emoji=f"{Emojis.link}",
 			url="https://github.com/FuncFusion/antbot"
+		))
+		self.add_item(discord.ui.Button(
+			label="Предложить идею/зарепортить баг",
+			emoji=f"{Emojis.edited_msg}",
+			url="https://discord.com/channels/914772142300749854/1276169141572730880"
 		))
 	
