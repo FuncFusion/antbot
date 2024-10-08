e84809211a23fa7065304abfb75bf43fdf2cd03e
SuperAnt220 <zp-a@i.ua>
2024-10-08 16:50:30 +0300

    ❔FAQ tweak, ping helpers temporarily disabled

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ logger = settings.logging.getLogger("bot")
 cogs = [DebugCommand, EditCommand, PingCommand, StatusCommands,
 		FAQs,
 		EnchantCommands, LookForCommand, RandomCommands,	
-		HelpCommand, LinkCommand, Pin, PingHelpers, ResolveCommand, StarterMessage, SyntaxCommand,
+		HelpCommand, LinkCommand, Pin, ResolveCommand, StarterMessage, SyntaxCommand,
 		JoinAndLeaveMessage, SayCommand, ServerInfoCommand, BotPing,
 		GiveawayCommand, GAModerationCommands,
 		Logs, IdeaVoteReactions,
 		ClearCommand, PunishmentCommands, MediaModeration,
 	    SnapshotScraper, ColorCommand, FileCommand, MessageFormatter, PackformatCommand, TemplateCommand,
 		CustomVoiceChannels,]
-views = [LookForView, R_u_sure, Ping_related_helpers, BotPingView]
+views = [LookForView, R_u_sure, BotPingView]
 
 
 class AntBot(commands.Bot):
